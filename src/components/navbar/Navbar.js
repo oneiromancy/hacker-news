b7ecbac531dc84f53d4e2de73b6ffd017dccd55a
oneiromancy <commaintel@gmail.com>
2020-01-25 22:12:14 +0000

    deleting authentication-related components

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,54 +1,46 @@
-import React from 'react';
-import {NavLink, withRouter} from 'react-router-dom';
-import queryString from 'query-string';
-import './Navbar.css';
-import MenuItem from './MenuItem';
-import AuthBtn from './AuthBtn';
+import React from "react";
+import { NavLink, withRouter } from "react-router-dom";
+import queryString from "query-string";
 
-const Navbar = ({location, isAuthenticated, logout}) => {
+import "./Navbar.css";
+
+import MenuItem from "./MenuItem";
+
+const Navbar = ({ location }) => {
     const userID = queryString.parse(location.search).id;
-    
-    return (
-        <div className = 'Navbar'> 
 
-            <NavLink 
-                exact to = '/' 
-                className = 'Navbar__logo Navbar__itemLink' 
-                activeClassName = 'Navbar__logo Navbar__menuItem--active'
+    return (
+        <div className="Navbar">
+            <NavLink
+                exact
+                to="/"
+                className="Navbar__logo Navbar__itemLink"
+                activeClassName="Navbar__logo Navbar__menuItem--active"
             >
                 Hacker News
             </NavLink>
 
+            <ul className="Navbar__menuList">
+                <MenuItem pathnameLink="/newest">new</MenuItem>
+                <MenuItem pathnameLink="/newcomments">comments</MenuItem>
+                <MenuItem pathnameLink="/ask">ask</MenuItem>
+                <MenuItem pathnameLink="/show">show</MenuItem>
+                <MenuItem pathnameLink="/jobs">jobs</MenuItem>
 
-            <ul className = 'Navbar__menuList'>
+                {location.pathname === "/submitted" && (
+                    <li className="Navbar__menuItem Navbar__menuItem--active">
+                        {`${userID}'s 'submissions`}
+                    </li>
+                )}
 
-                <MenuItem pathnameLink = '/newest'>new</MenuItem>
-                <MenuItem pathnameLink = '/newcomments'>comments</MenuItem>
-                <MenuItem pathnameLink = '/ask'>ask</MenuItem>
-                <MenuItem pathnameLink = '/show'>show</MenuItem>
-                <MenuItem pathnameLink = '/jobs'>jobs</MenuItem>
-                <MenuItem pathnameLink = '/submit'>submit</MenuItem>
-
-                {   location.pathname === '/submitted' &&
-                    <li className = 'Navbar__menuItem Navbar__menuItem--active'>
-                        {`${userID}'s 'submissions`} 
-                    </li>               
-                }
-
-                {   location.pathname === '/threads' &&
-                    <li className = 'Navbar__menuItem Navbar__menuItem--active'>
-                        {`${userID}'s 'comments`} 
-                    </li>               
-                }
-
+                {location.pathname === "/threads" && (
+                    <li className="Navbar__menuItem Navbar__menuItem--active">
+                        {`${userID}'s 'comments`}
+                    </li>
+                )}
             </ul>
-
-            <div className = 'Navbar__authBtn'>
-                <AuthBtn isAuthenticated = {isAuthenticated} logout = {logout}/>
-            </div>
-
         </div>
-    )
-}
+    );
+};
 
 export default withRouter(Navbar);
