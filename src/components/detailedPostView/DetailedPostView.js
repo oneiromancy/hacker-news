5909a363dba8e6ecb86fd46e71f38a604b8d2c23
oneiromancy <commaintel@gmail.com>
2020-01-25 22:09:54 +0000

    deleting comment form component

--- src/components/detailedPostView/DetailedPostView.js
+++ src/components/detailedPostView/DetailedPostView.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import Story from '../story/Story';
-import {CommentList} from '../commentList/CommentList';
-import Comment from '../comment/Comment';
-import CommentForm from '../commentForm/CommentForm';
-import countComments from '../../utils/countComments';
-import withItemData from '../../hoc/withItemData';
+import React from "react";
 
-const DetailedPostView = ({data, isAuthenticated}) =>
-    <> 
-        { data.type !== 'comment'?
+import countComments from "../../utils/countComments";
+
+import "./DetailedPostView.css";
+
+import Story from "../story/Story";
+import CommentList from "../commentList/CommentList";
+import Comment from "../comment/Comment";
+
+const DetailedPostView = ({ data }) => (
+    <div className="DetailedPostView">
+        {data.type == "story" ? (
             <>
-                <Story
-                    {...data}
-                    num_comments = {countComments(data)}
-                />
-                <CommentForm
-                    isAuthenticated = {isAuthenticated}
-                />
-                <CommentList 
-                    data = {data.children}
-                />
-            </>:
-            <>
-                <Comment {...data}/>
-                <CommentForm/>
+                <Story {...data} num_comments={countComments(data)} />
+
+                <div>
+                    <h1 className="DetailedPostView__commentsHeading">
+                        Comments
+                    </h1>
+                    <CommentList data={data.children} />
+                </div>
             </>
-        }
-    </>
+        ) : (
+            <Comment {...data} />
+        )}
+    </div>
+);
 
-export default withItemData(DetailedPostView);
+export default DetailedPostView;
