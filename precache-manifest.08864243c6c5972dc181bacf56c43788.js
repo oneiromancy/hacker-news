36af8fbbee903e30d8d17a0cdac7a39fc80da755
Henri <oneiromancy.ai@gmail.com>
2021-01-30 21:32:28 +0000

    Updates

---
+++ precache-manifest.08864243c6c5972dc181bacf56c43788.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca33a5a61e3e2c65e571",
+    "url": "/hacker-news/static/css/main.8cac74ec.chunk.css"
+  },
+  {
+    "revision": "ca33a5a61e3e2c65e571",
+    "url": "/hacker-news/static/js/main.ca33a5a6.chunk.js"
+  },
+  {
+    "revision": "388ec0cd24e6422163cb",
+    "url": "/hacker-news/static/js/runtime~main.388ec0cd.js"
+  },
+  {
+    "revision": "c0b1b374b9ab34eeecad",
+    "url": "/hacker-news/static/js/2.c0b1b374.chunk.js"
+  },
+  {
+    "revision": "7a48b806380ab6b7901d000a8f8fab4d",
+    "url": "/hacker-news/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c5c2360f0f9bce22968a910a257e4e6.js"
+  "/hacker-news/precache-manifest.08864243c6c5972dc181bacf56c43788.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/hacker-news/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
